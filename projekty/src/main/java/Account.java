1847b46e290c6100641fb396ce04b50d7e743849
Dariusz <d.kiwak@wp.pl>
2022-03-14 19:48:53 +0100

    Add accunt and main class

--- projekty/src/main/java/Account.java
+++ projekty/src/main/java/Account.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Account {
 
     private int accountNo;
     private String accountName;
     private String accountSurname;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public Account(int accountNo, String accountName, String accountSurname, long balance) {
         this.accountNo = accountNo;
@@ -25,6 +29,17 @@ public class Account {
 
         }
 
+        public void openAccount(){
+            System.out.println("podaj numer konta");
+            accountNo= scanner.nextInt();
+            System.out.println("podaj imię");
+            accountName= scanner.next();
+            System.out.println("podaj nazwisko");
+            accountSurname= scanner.next();
+            System.out.println("podaj balance");
+            balance= scanner.nextInt();
+
+        }
 
     }
 
